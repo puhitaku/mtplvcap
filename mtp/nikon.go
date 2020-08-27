3a667c2dd0ddfec481925ccc05171fdb23832e69
Takumi Sueda <puhitaku@gmail.com>
2020-08-27 17:39:43 +0900

    Add models

--- mtp/nikon.go
+++ mtp/nikon.go
@@ -59,12 +59,72 @@ var models = ModelMap{
 		Name: "Generic",
 		HeaderSize: 384,
 	},
+	"D3": {
+		Name: "D3",
+		HeaderSize: 128,
+	},
+	"D3s": {
+		Name: "D3s",
+		HeaderSize: 128,
+	},
+	"D3X": {
+		Name: "D3X",
+		HeaderSize: 64,
+	},
+	"D300": {
+		Name: "D300",
+		HeaderSize: 64,
+	},
+	"D3200": {
+		Name: "D3200",
+		HeaderSize: 384,
+	},
+	"D3300": {
+		Name: "D3300",
+		HeaderSize: 384,
+	},
 	"D5000": {
 		Name: "D5000",
 		HeaderSize: 128,
 	},
 	"D5300": {
 		Name: "D5300",
+		HeaderSize: 384,
+	},
+	"D5500": {
+		Name: "D5500",
+		HeaderSize: 384,
+	},
+	"D600": {
+		Name: "D600",
+		HeaderSize: 384,
+	},
+	"D610": {
+		Name: "D610",
+		HeaderSize: 384,
+	},
+	"D700": {
+		Name: "D700",
+		HeaderSize: 64,
+	},
+	"D7000": {
+		Name: "D7000",
+		HeaderSize: 384,
+	},
+	"D7200": {
+		Name: "D7200",
+		HeaderSize: 384,
+	},
+	"D90": {
+		Name: "D90",
+		HeaderSize: 128,
+	},
+	"Z6": {
+		Name: "Z6",
+		HeaderSize: 384,
+	},
+	"Z7": {
+		Name: "Z7",
 		HeaderSize: 384,
 	},
 }
