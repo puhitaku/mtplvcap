a738cc7327a02c8fdbc07c0a8b7f4435ae0a91b7
Takumi Sueda <puhitaku@gmail.com>
2021-06-26 00:19:04 +0900

    Fix (potential) switch media quirk
    Older models are prone to need switching media

--- mtp/nikon.go
+++ mtp/nikon.go
@@ -70,18 +70,22 @@ var models = ModelMap{
 	"D3": {
 		Name:       "D3",
 		HeaderSize: 128,
+		QuirkSwitchMedia: true,
 	},
 	"D3s": {
 		Name:       "D3s",
 		HeaderSize: 128,
+		QuirkSwitchMedia: true,
 	},
 	"D3X": {
 		Name:       "D3X",
 		HeaderSize: 64,
+		QuirkSwitchMedia: true,
 	},
 	"D300": {
 		Name:       "D300",
 		HeaderSize: 64,
+		QuirkSwitchMedia: true,
 	},
 	"D3200": {
 		Name:       "D3200",
