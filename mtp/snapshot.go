8c5d20a799af364ba90606639e62b3b3690b6621
Stephen Craton <stephen@craton.dev>
2022-08-19 18:39:03 +0000

    Return last recorded frame for snapshot endpoint

--- mtp/snapshot.go
+++ mtp/snapshot.go
@@ -1,32 +1,22 @@
 package mtp
 
 import (
-	"context"
 	"net/http"
-	"time"
 )
 
 type SnapshotResponseWriter struct {
-	Context context.Context
-	cancel  context.CancelFunc
-	w       http.ResponseWriter
+	w http.ResponseWriter
 }
 
 func NewSnapshotResponseWriter(w http.ResponseWriter) *SnapshotResponseWriter {
-	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-	ctx, cancel = context.WithCancel(ctx)
-
 	return &SnapshotResponseWriter{
-		Context: ctx,
-		cancel:  cancel,
-		w:       w,
+		w: w,
 	}
 }
 
 func (s *SnapshotResponseWriter) Write(jpeg []byte) error {
 	s.w.Header().Set("Content-Type", "image/jpeg")
 	s.w.Write(jpeg)
-	s.cancel()
 
 	return nil
 }
